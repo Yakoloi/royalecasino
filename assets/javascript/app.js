d91f7aa86579cd122e20873b3876692a5155bbf6
Yakoloi <yakoloi@gmail.com>
2017-07-31 18:38:25 -0700

    added pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,31 @@
+//implement a start screen
+  // click to start
+
+//enter login screen
+  // sign up or login
+  // sign up ask for email and password
+  // sign up ask for oAuth (Google, Facebook etc)
+  // ask to create a username
+  // else
+  // login using email username or oAuth
+  // record user info
+
+//enter the main menu
+// map on click button to start game(single)
+  // enter game screen
+  // click to start
+
+// map on click button to start game(multi)
+  //enter game screen
+  // click on board position to ready
+    // starts multiplayer chat
+  
+// map on click button to help screen
+  // enter a single sheet help pop up screen
+  // click to exit
+  
+// map on click button to leaderboard
+  // enter a dynamically created leaderboard
+  // leaderboard adds username and score and flair sorted by score
+  // button click to exit
+
